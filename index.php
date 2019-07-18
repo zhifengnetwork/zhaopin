7bdfe9302ed599e1cd2888b1fdc392b0645cb891
李柏成 <729127678@qq.com>
2019-07-18 17:56:51 +0800

    index

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+// [ 应用入口文件 ]
+
+define('HTTP_HOST', $_SERVER['HTTP_HOST']);
+
+$http = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] && $_SERVER['HTTPS'] != 'off') ? 'https' : 'http';
+define('SITE_URL',$http.'://'.$_SERVER['HTTP_HOST']); // 网站域名
+// 定义时间
+define('NOW_TIME',$_SERVER['REQUEST_TIME']);
+// 定义应用目录
+define('APP_PATH', __DIR__ . '/application/');
+// 加载框架引导文件
+require __DIR__ . '/thinkphp/start.php';

d52ee17ddf6a2de62efee39e0c3a195aa7cb2ef1
李柏成 <729127678@qq.com>
2019-07-24 18:43:27 +0800

    1

--- application/api/controller/Collection.php
+++ application/api/controller/Collection.php
@@ -21,14 +21,15 @@ class Collection extends ApiBase
         if(!$regtype){
             $this->ajaxReturn(['status' => -2 , 'msg'=>'参数错误','data'=>'']);
         }
-        $type=Db::name('member')->where('id',$user_id)->value('regtype');
-        if($type==3){
+//        $type=Db::name('member')->where('id',$user_id)->value('regtype');
+        if($regtype==1){
             $list = Db::table('collection')->alias('c')
                 ->join('recruit r','r.id=c.to_id','LEFT')
                 ->join('company co','co.id=r.company_id','LEFT')
                 ->join('member m','m.id=co.user_id','LEFT')
                 ->join('category ca','ca.cat_id=r.type','LEFT')
                 ->field('r.id,r.title,ca.cat_name,r.work_age,r.require_cert,r.salary,co.logo,m.regtype')
+                ->where('c.type',1)
                 ->where('c.user_id',$user_id)
                 ->where('m.regtype',$regtype)
                 ->select();
@@ -37,12 +38,29 @@ class Collection extends ApiBase
 //                $list[$key]['district']=Region::getName($list[$key]['district']);
 //            }
             $this->ajaxReturn(['status' => 1 , 'msg'=>'成功！','data'=>$list]);
-        }else{//公司类型收藏
+        }elseif ($regtype==2){
+            $list = Db::table('collection')->alias('c')
+                ->join('recruit r','r.id=c.to_id','LEFT')
+                ->join('company co','co.id=r.company_id','LEFT')
+                ->join('member m','m.id=co.user_id','LEFT')
+                ->join('category ca','ca.cat_id=r.type','LEFT')
+                ->field('r.id,r.title,ca.cat_name,r.work_age,r.require_cert,r.salary,co.logo,m.regtype')
+                ->where('c.type',1)
+                ->where('c.user_id',$user_id)
+                ->where('m.regtype',$regtype)
+                ->select();
+//            foreach ($list as $key=>$value){
+//                $list[$key]['city']=Region::getName($list[$key]['city']);
+//                $list[$key]['district']=Region::getName($list[$key]['district']);
+//            }
+            $this->ajaxReturn(['status' => 1 , 'msg'=>'成功！','data'=>$list]);
+        }elseif ($regtype==3){
             $list = Db::table('collection')->alias('c')
                 ->join('person p','p.id=c.to_id','LEFT')
                 ->join('member m','m.id=p.user_id','LEFT')
                 ->join('category ca','ca.cat_id=p.job_type','LEFT')
                 ->field('p.id,p.name,p.desc,ca.cat_name,p.work_age,p.images,p.avatar,m.regtype')
+                ->where('c.type',1)
                 ->where('c.user_id',$user_id)
                 ->where('m.regtype',$regtype)
                 ->select();
@@ -51,6 +69,37 @@ class Collection extends ApiBase
             }
             $this->ajaxReturn(['status' => 1 , 'msg'=>'成功！','data'=>$list]);
         }
+//        if($type==3){
+//            $list = Db::table('collection')->alias('c')
+//                ->join('recruit r','r.id=c.to_id','LEFT')
+//                ->join('company co','co.id=r.company_id','LEFT')
+//                ->join('member m','m.id=co.user_id','LEFT')
+//                ->join('category ca','ca.cat_id=r.type','LEFT')
+//                ->field('r.id,r.title,ca.cat_name,r.work_age,r.require_cert,r.salary,co.logo,m.regtype')
+//                ->where('c.type',2)
+//                ->where('c.user_id',$user_id)
+//                ->where('m.regtype',$regtype)
+//                ->select();
+////            foreach ($list as $key=>$value){
+////                $list[$key]['city']=Region::getName($list[$key]['city']);
+////                $list[$key]['district']=Region::getName($list[$key]['district']);
+////            }
+//            $this->ajaxReturn(['status' => 1 , 'msg'=>'成功！','data'=>$list]);
+//        }else{//公司类型收藏
+//            $list = Db::table('collection')->alias('c')
+//                ->join('person p','p.id=c.to_id','LEFT')
+//                ->join('member m','m.id=p.user_id','LEFT')
+//                ->join('category ca','ca.cat_id=p.job_type','LEFT')
+//                ->field('p.id,p.name,p.desc,ca.cat_name,p.work_age,p.images,p.avatar,m.regtype')
+//                ->where('c.type',1)
+//                ->where('c.user_id',$user_id)
+//                ->where('m.regtype',$regtype)
+//                ->select();
+//            foreach ($list as $key=>$value){
+//                $list[$key]['images']=$list[$key]['images']?1:0;
+//            }
+//            $this->ajaxReturn(['status' => 1 , 'msg'=>'成功！','data'=>$list]);
+//        }
 
     }
 

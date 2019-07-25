feeef32aecb573fd6de2c72601d6c18827ab666c
李柏成 <729127678@qq.com>
2019-07-25 14:11:43 +0800

    消息与微信绑定

--- application/api/controller/Login.php
+++ application/api/controller/Login.php
@@ -15,7 +15,7 @@ use think\Session;
 use think\captcha\Captcha;
 use app\common\util\jwt\JWT;
 
-class Login extends \think\Controller
+class Login extends ApiBase
 {
 
 
@@ -59,56 +59,55 @@ class Login extends \think\Controller
         }
 
     }
-
-
     /**
-     * 登录接口
+     * 微信登录
      */
-    public function login()
-    {
+    public function bind_weixin () {
+        $user_id=$this->get_user_id();
+        if(!$user_id){
+            $this->ajaxReturn(['status' => -1 , 'msg'=>'用户不存在','data'=>'']);
+        }
+        $code = input('code');
+        if(!$code){
+            $this->ajaxReturn(['status' => -1 , 'msg'=>'code不能为空','data'=>'']);
+        }
 
-        header('Access-Control-Allow-Origin:*');
-        header('Access-Control-Allow-Headers:*');
-        header("Access-Control-Allow-Methods:GET, POST, OPTIONS, DELETE");
-        header('Content-Type:application/json; charset=utf-8');
+        $appid = Db::name('config')->where(['name'=>'appid'])->value('value');
+        $appsecret = Db::name('config')->where(['name'=>'appsecret'])->value('value');
 
-        $mobile    = input('mobile');
-        $password1 = input('password');
-        $password  = md5('TPSHOP'.$password1);
+        $url = 'https://api.weixin.qq.com/sns/jscode2session?appid='.$appid.'&secret='.$appsecret.'&js_code='.$code.'&grant_type=authorization_code' ;
+        $result = httpRequest($url, 'GET');
+        $arr = json_decode($result, true);
+        if(!isset($arr['openid'])){
+            $this->ajaxReturn(['status' => -1 , 'msg'=>$arr['errmsg'],'data'=>'']);
+        }
 
-        $data = Db::name("users")->where('mobile',$mobile)
-            ->field('password,user_id')
-            ->find();
+        $openid = $arr['openid'];
 
+        // 查询数据库，判断是否有此openid
+        $data = Db::table('member')->where('openid',$openid)->find();
         if(!$data){
-            exit(json_encode(['status' => -1 , 'msg'=>'手机不存在或错误','data'=>null]));
+            $res=Db::name('member')->where(['id'=>$user_id])->update(['openid'=>$openid]);
+            if($res){
+                $this->ajaxReturn(['status' => 1 , 'msg'=>'绑定成功','data'=>$data]);
+            }else{
+                $this->ajaxReturn(['status' => 1 , 'msg'=>'绑定失败','data'=>$data]);
+            }
+        }else{
+
+//            $data['token'] = $this->create_token($data['id']);
+            if($user_id==$data['id']){
+                $this->ajaxReturn( ['status'=>-2,'msg'=>'您已绑定该微信，请勿重新绑定','data'=>$data]);
+            }else{
+                $this->ajaxReturn( ['status'=>-2,'msg'=>'该微信已绑定其他账户','data'=>$data]);
+            }
+
+
         }
-        if ($password != $data['password']) {
-            exit(json_encode(['status' => -2 , 'msg'=>'登录密码错误','data'=>null]));
-        }
-        unset($data['password']);
-        //重写
-        $data['token'] = $this->create_token($data['user_id']);
-        
-        exit(json_encode(['status' => 0 , 'msg'=>'登录成功','data'=>$data],JSON_UNESCAPED_UNICODE));
 
     }
 
-    /**
-     * 生成token
-     */
-    private function create_token($user_id){
-        $time = time();
-        $payload = array(
-            "iss"=> "DC",
-            "iat"=> $time ,
-            "exp"=> $time + 36000 ,
-            "user_id"=> $user_id
-        );
-        $key = 'zhelishimiyao';
-        $token = JWT::encode($payload, $key, $alg = 'HS256', $keyId = null, $head = null);
-        return $token;
-    }
+
 
 
 
---
+++ application/api/controller/Message.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MyPC
+ * Date: 2019/4/19
+ * Time: 11:49
+ */
+
+namespace app\api\controller;
+use think\Db;
+
+class Message extends ApiBase
+{
+
+    public function message_list(){
+        $user_id=$this->get_user_id();
+        if(!$user_id){
+            $this->ajaxReturn(['status' => -1 , 'msg'=>'用户不存在','data'=>'']);
+        }
+        $message=Db::name('message')->where(['show'=>1])->select();
+        if (!empty($message)){
+            return json(['code'=>1,'msg'=>'获取成功','data'=>$message]);
+        }else{
+            return json(['code'=>0,'msg'=>'没有数据哦','data'=>$message]);
+        }
+    }
+    public function message_detail(){
+        $user_id=$this->get_user_id();
+        if(!$user_id){
+            $this->ajaxReturn(['status' => -1 , 'msg'=>'用户不存在','data'=>'']);
+        }
+        $id=input('id');
+        $message=Db::name('message')->where(['show'=>1,'id'=>$id])->find();
+        if (!empty($message)){
+            return json(['code'=>1,'msg'=>'获取成功','data'=>$message]);
+        }else{
+            return json(['code'=>0,'msg'=>'没有数据哦','data'=>$message]);
+        }
+    }
+}
